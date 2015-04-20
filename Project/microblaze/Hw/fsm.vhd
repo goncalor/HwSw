a5ef9ac9b4b21e440460b9597ffe8b0e890c6a9f
Luís Fiolhais <luisazenhas.fiolhais@gmail.com>
2015-04-20 14:11:34 +0100

    Máquina de Estados funciona. Testado em comportamental.

--- Project/microblaze/Hw/fsm.vhd
+++ Project/microblaze/Hw/fsm.vhd
@@ -6,65 +6,77 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 entity fsm is
 	port(
 			-- Input
-			clk	: in std_logic;
-			rst	: in std_logic;
+			clk	  : in std_logic;
+			rst	  : in std_logic;
+			start : in std_logic;
 
 			-- Output
-			sel_word : out std_logic_vector(1 downto 0)
+			sel_word : out std_logic_vector(1 downto 0);
+			we       : out std_logic;
+			comp_en  : out std_logic
 			);
 end fsm;
 
 architecture Behavioral of fsm is
 ---------------- Auxilliary Variables ----------------
-	type fsm_states is (s_initial, s_write_char, s_end);
+	type fsm_states is (s_initial, s_write_char_1, s_write_char_2, s_write_char_3, s_write_char_4, s_end);
 	signal currstate, nextstate: fsm_states;
-	signal count : integer;
-	constant max_words : integer := 4;
 
 begin
 --------------- Initialize Machine ------------------
 	state_reg : process(clk, rst) -- Machine starts with reset signal
 	begin
-		if rst = '1' then
-			currstate <= s_initial;
-		elsif clk'event and clk = '1' then
-			currstate <= nextstate;
-		end if;
+		if clk'event and clk = '1' then
+			if rst = '1' then
+				currstate <= s_initial;
+			else
+				currstate <= nextstate;
+			end if;
+		end if ;
 	end process ; -- state_reg
 
 ----------------- Machine States --------------------
-	state_comb : process(currstate, count)
+	state_comb : process(currstate, clk, start)
 	begin
 		nextstate <= currstate;
-		count <= 0;
+		we <= '0';
+		comp_en <= '0';
+		sel_word <= "00";
 
 		case(currstate) is
 			------- Initial State -------------
 			when s_initial => -- Initial State
-				if count /= max_words then
-					nextstate <= s_write_char;
-					count <= count + 1;
-				else
-					nextstate <= s_end;
-					count <= 0;
+				if start = '1' then
+					nextstate <= s_write_char_1;
+					we <= '0';
+					comp_en <= '0';
+					sel_word <= "00";
 				end if;
 
 			------- Write Char to Memory ------
-			when s_write_char =>
-				case(count) is
-					when 0 =>
-						nextstate <= s_initial;
-						sel_word <= "00";
-					when 1 =>
-						nextstate <= s_initial;
-						sel_word <= "01";
-					when 2 =>
-						nextstate <= s_initial;
-						sel_word <= "10";
-					when others =>
-						nextstate <= s_initial;
-						sel_word <= "11";
-				end case ;
+			when s_write_char_1 =>
+			sel_word <= "00";
+			we <= '1';
+			comp_en <= '0';
+			nextstate <= s_write_char_2;
+
+			when s_write_char_2 =>
+			sel_word <= "01";
+			we <= '1';
+			comp_en <= '1';
+			nextstate <= s_write_char_3;
+
+			when s_write_char_3 =>
+			sel_word <= "10";
+			we <= '1';
+			comp_en <= '1';
+			nextstate <= s_write_char_4;
+
+			when s_write_char_4 =>
+			sel_word <= "11";
+			we <= '1';
+			comp_en <= '1';
+			nextstate <= s_end;
 
 			------- Final State -------------
 			when s_end =>
