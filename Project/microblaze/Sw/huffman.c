28c81af169981c0a7b7d15808126e8e840c44185
Gonçalo Ribeiro <darklaw13@yahoo.co.uk>
2015-04-25 22:10:24 +0100

    Tiny corrections.

--- Project/microblaze/Sw/huffman.c
+++ Project/microblaze/Sw/huffman.c
@@ -7,7 +7,7 @@
 #include "file.h"
 
 #ifdef MB
-#include "htimer.h"
+//#include "htimer.h"
 #include "fsl.h"
 #endif
 
@@ -21,9 +21,9 @@ int main(int argc, char **argv)
 	char * out;
 	#else
 	char *file = (char *) (0xa8100000);
-	u32 timeL[11], timeH[11];
-	init_timer(1);
-	start_timer(1);
+	//u32 timeL[11], timeH[11];
+	//init_timer(1);
+	//start_timer(1);
 	#endif
 
 
@@ -39,7 +39,7 @@ int main(int argc, char **argv)
 	#endif
 
 	#ifdef MB
-	timeH[0] = get_timer64_val(&(timeL[0]));
+	//timeH[0] = get_timer64_val(&(timeL[0]));
 	#endif
 
 	file[MAX_FILE_SIZE-1] = FILE_END_CODE;	// place end of file code. to be safe
@@ -47,7 +47,6 @@ int main(int argc, char **argv)
 	#ifndef MB
 	compute_stats((char *) file);
 	#else
-
 	u32 * file_aux = (u32 *) file;
 
 	cputfsl(FILE_END_CODE, 0);	// send FILE_END_CODE for the accelarator to recognise it
@@ -68,14 +67,15 @@ int main(int argc, char **argv)
 	{
 		getfsl(tmp, 0);
 
-		stats[i] = tmp & 0xFFFF0000>>16;
+		stats[i] = (tmp & 0xFFFF0000) >> 16;
 		stats[i + 1] = tmp & 0x0000FFFF;
 
 		xil_printf("stats %d -> %d\n", i, stats[i]);
 		xil_printf("stats %d -> %d\n", i + 1, stats[i + 1]);
 	}
 
-	timeH[1] = get_timer64_val(&(timeL[1]));
+	stats[FILE_END_CODE] = 1;
+	//timeH[1] = get_timer64_val(&(timeL[1]));
 	#endif
 
 	#ifdef debug
@@ -119,27 +119,27 @@ int main(int argc, char **argv)
 	#endif
 
 	#ifdef MB
-	timeH[2] = get_timer64_val(&(timeL[2]));
+	//timeH[2] = get_timer64_val(&(timeL[2]));
 	#endif
 
 	MinHeapNode* huffman_tree = buildHuffmanTree(ascii, (unsigned *) &stats, size);
 
 	#ifdef MB
-	timeH[3] = get_timer64_val(&(timeL[3]));
+	//timeH[3] = get_timer64_val(&(timeL[3]));
 	#endif
 
 	#ifdef MB
-	timeH[4] = get_timer64_val(&(timeL[4]));
+	//timeH[4] = get_timer64_val(&(timeL[4]));
 	#endif
 
-	//HuffmanPrint(huffman_tree, (char *)file);
+	HuffmanPrint(huffman_tree, (char *)file);
 
 	#ifdef MB
-	timeH[5] = get_timer64_val(&(timeL[5]));
+	//timeH[5] = get_timer64_val(&(timeL[5]));
 	#endif
 
 	#ifdef MB
-	timeH[6] = get_timer64_val(&(timeL[6]));
+	//timeH[6] = get_timer64_val(&(timeL[6]));
 	#endif
 
 	// let's reuse the stats array as a table for the codewords obtained from the tree
@@ -147,27 +147,27 @@ int main(int argc, char **argv)
 	tree_to_table(huffman_tree, encoding_table, 0, 1);
 
 	#ifdef MB
-	timeH[7] = get_timer64_val(&(timeL[7]));
+	//timeH[7] = get_timer64_val(&(timeL[7]));
 	#endif
 
 	#ifdef MB
-	timeH[8] = get_timer64_val(&(timeL[8]));
+	//timeH[8] = get_timer64_val(&(timeL[8]));
 	#endif
 
 	// encode the buffer
 	unsigned outbuf_len = encode_file((char *)file, (char *)file, encoding_table);
 
 	#ifdef MB
-	timeH[9] = get_timer64_val(&(timeL[9]));
+	//timeH[9] = get_timer64_val(&(timeL[9]));
 	#endif
 
 	#ifndef MB
 	out = strcat(argv[1], "_comp");
 	write_file(out, file, outbuf_len);
 	#else
-	timeH[10] = get_timer64_val(&(timeL[10]));
+	//timeH[10] = get_timer64_val(&(timeL[10]));
 
-	xil_printf("start compute_stats(): %d ms\n", (int) conv2_cycles_to_msecs(timeH[0], timeL[0]));
+	/*xil_printf("start compute_stats(): %d ms\n", (int) conv2_cycles_to_msecs(timeH[0], timeL[0]));
 	xil_printf("finish compute_stats(): %d ms\n", (int) conv2_cycles_to_msecs(timeH[1], timeL[1]));
 	xil_printf("start buildHuffmanTree(): %d ms\n", (int) conv2_cycles_to_msecs(timeH[2], timeL[2]));
 	xil_printf("finish buildHuffmanTree(): %d ms\n", (int) conv2_cycles_to_msecs(timeH[3], timeL[3]));
@@ -177,7 +177,7 @@ int main(int argc, char **argv)
 	xil_printf("finish tree_to_table(): %d ms\n", (int) conv2_cycles_to_msecs(timeH[7], timeL[7]));
 	xil_printf("start encode_file(): %d ms\n", (int) conv2_cycles_to_msecs(timeH[8], timeL[8]));
 	xil_printf("finish encode_file(): %d ms\n", (int) conv2_cycles_to_msecs(timeH[9], timeL[9]));
-	xil_printf("time elapsed: %d ms\n", (int) conv2_cycles_to_msecs(timeH[10], timeL[10]));
+	xil_printf("time elapsed: %d ms\n", (int) conv2_cycles_to_msecs(timeH[10], timeL[10]));*/
 	#endif
 
 	// Print statistics of compression
